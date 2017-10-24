9c98eb54fc1ae36c84c2450d9d50646b0b232a45
ZhangJieBo <530092772@qq.com>
2017-10-24 18:20:47 +0800

    卖车

--- app/src/main/java/com/haoche666/buyer/fragment/SellCheFragment.java
+++ app/src/main/java/com/haoche666/buyer/fragment/SellCheFragment.java
@@ -1,14 +1,18 @@
 package com.haoche666.buyer.fragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.haoche666.buyer.R;
+import com.haoche666.buyer.base.ZjbBaseFragment;
+import com.haoche666.buyer.util.ScreenUtils;
 
-public class SellCheFragment extends Fragment {
+public class SellCheFragment extends ZjbBaseFragment {
+
+    private View mInflate;
+    private View mRelaTitleStatue;
 
     public SellCheFragment() {
         // Required empty public constructor
@@ -19,7 +23,48 @@ public class SellCheFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sell_che, container, false);
+        if (mInflate == null) {
+            mInflate = inflater.inflate(R.layout.fragment_sell_che, container, false);
+            init();
+        }
+        //缓存的rootView需要判断是否已经被加过parent， 如果有parent需要从parent删除，要不然会发生这个rootview已经有parent的错误。
+        ViewGroup parent = (ViewGroup) mInflate.getParent();
+        if (parent != null) {
+            parent.removeView(mInflate);
+        }
+        return mInflate;
     }
 
+    @Override
+    protected void initIntent() {
+
+    }
+
+    @Override
+    protected void initSP() {
+
+    }
+
+    @Override
+    protected void findID() {
+        mRelaTitleStatue = mInflate.findViewById(R.id.relaTitleStatue);
+    }
+
+    @Override
+    protected void initViews() {
+        ViewGroup.LayoutParams layoutParams = mRelaTitleStatue.getLayoutParams();
+        layoutParams.height = (int) (getResources().getDimension(R.dimen.titleHeight) + ScreenUtils.getStatusBarHeight(getActivity()));
+        mRelaTitleStatue.setLayoutParams(layoutParams);
+        mRelaTitleStatue.setPadding(0, ScreenUtils.getStatusBarHeight(getActivity()), 0, 0);
+    }
+
+    @Override
+    protected void setListeners() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
 }
