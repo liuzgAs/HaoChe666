3a010b987712501890ef6cbd6a9cece4a136916f
ZhangJieBo <530092772@qq.com>
2017-10-26 16:01:40 +0800

    查维保

--- app/src/main/java/com/haoche666/buyer/avtivity/ChaWeiBaoActivity.java
+++ app/src/main/java/com/haoche666/buyer/avtivity/ChaWeiBaoActivity.java
@@ -1,6 +1,5 @@
 package com.haoche666.buyer.avtivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -8,12 +7,12 @@ import android.widget.TextView;
 import com.haoche666.buyer.R;
 import com.haoche666.buyer.base.ZjbBaseActivity;
 
-public class ChaXunFWActivity extends ZjbBaseActivity implements View.OnClickListener {
+public class ChaWeiBaoActivity extends ZjbBaseActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cha_xun_fw);
+        setContentView(R.layout.activity_cha_wei_bao);
         init();
     }
 
@@ -34,13 +33,12 @@ public class ChaXunFWActivity extends ZjbBaseActivity implements View.OnClickLis
 
     @Override
     protected void initViews() {
-        ((TextView) findViewById(R.id.textViewTitle)).setText("查询服务");
+        ((TextView) findViewById(R.id.textViewTitle)).setText("查维保");
     }
 
     @Override
     protected void setListeners() {
         findViewById(R.id.imageBack).setOnClickListener(this);
-        findViewById(R.id.viewChaWeiXiu).setOnClickListener(this);
     }
 
     @Override
@@ -51,11 +49,6 @@ public class ChaXunFWActivity extends ZjbBaseActivity implements View.OnClickLis
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.viewChaWeiXiu:
-                Intent intent = new Intent();
-                intent.setClass(this, ChaWeiBaoActivity.class);
-                startActivity(intent);
-                break;
             case R.id.imageBack:
                 finish();
                 break;
