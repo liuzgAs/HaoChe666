fe910f8a16cbe4b159a95fdde443e957b1cedc41
ZhangJieBo <530092772@qq.com>
2017-11-25 16:44:26 +0800

    视频看车dialog

--- app/src/main/java/com/haoche666/buyer/avtivity/CheLiangXQActivity.java
+++ app/src/main/java/com/haoche666/buyer/avtivity/CheLiangXQActivity.java
@@ -2,13 +2,19 @@ package com.haoche666.buyer.avtivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.bigkoo.convenientbanner.ConvenientBanner;
@@ -41,6 +47,7 @@ public class CheLiangXQActivity extends ZjbBaseActivity implements SwipeRefreshL
     private TextView textViewTitle;
     private int viewBarHeight;
     private List<Integer> imgList = new ArrayList<>();
+    private AlertDialog payVideoDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,9 +111,14 @@ public class CheLiangXQActivity extends ZjbBaseActivity implements SwipeRefreshL
         });
         adapter.addHeader(new RecyclerArrayAdapter.ItemView() {
 
+            private TextView textVip;
+            private TextView textSingle;
+            private View viewSingle;
+            private View viewVip;
             private TextView textZhiShiQi;
             private ConvenientBanner banner;
             private List<Integer> imgList = new ArrayList<>();
+            private boolean isVip = true;
 
             @Override
             public View onCreateView(ViewGroup parent) {
@@ -129,7 +141,7 @@ public class CheLiangXQActivity extends ZjbBaseActivity implements SwipeRefreshL
 
                     @Override
                     public void onPageSelected(int position) {
-                        textZhiShiQi.setText((position+1) + "/" + imgList.size());
+                        textZhiShiQi.setText((position + 1) + "/" + imgList.size());
                     }
 
                     @Override
@@ -137,7 +149,75 @@ public class CheLiangXQActivity extends ZjbBaseActivity implements SwipeRefreshL
 
                     }
                 });
+                header_che_liang_xq.findViewById(R.id.buttonVR).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        payVideo();
+                    }
+                });
                 return header_che_liang_xq;
+            }
+
+            /**
+             * 支付视频dialog
+             */
+            private void payVideo() {
+                isVip = true;
+                LayoutInflater inflater = LayoutInflater.from(CheLiangXQActivity.this);
+                View dialog_chan_pin = inflater.inflate(R.layout.dialog_pay_video, null);
+                dialog_chan_pin.findViewById(R.id.imageCancle).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        payVideoDialog.dismiss();
+                    }
+                });
+                viewVip = dialog_chan_pin.findViewById(R.id.viewVip);
+                viewSingle = dialog_chan_pin.findViewById(R.id.viewSingle);
+                textSingle = dialog_chan_pin.findViewById(R.id.textSingle);
+                textVip = dialog_chan_pin.findViewById(R.id.textVip);
+                viewVip.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        isVip = true;
+                        vipRadio();
+                    }
+                });
+                viewSingle.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        isVip = false;
+                        vipRadio();
+                    }
+                });
+                vipRadio();
+                payVideoDialog = new AlertDialog.Builder(CheLiangXQActivity.this, R.style.dialog)
+                        .setView(dialog_chan_pin)
+                        .create();
+                payVideoDialog.show();
+                Window dialogWindow = payVideoDialog.getWindow();
+                dialogWindow.setGravity(Gravity.BOTTOM);
+                dialogWindow.setWindowAnimations(R.style.dialogFenXiang);
+                WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+                DisplayMetrics d = CheLiangXQActivity.this.getResources().getDisplayMetrics(); // 获取屏幕宽、高用
+                lp.width = (int) (d.widthPixels * 1); // 高度设置为屏幕的0.6
+                dialogWindow.setAttributes(lp);
+            }
+
+            /**
+             * 切换开通vip和单个视频
+             */
+            private void vipRadio() {
+                if (isVip) {
+                    viewSingle.setBackgroundResource(R.drawable.shape_gray1dp_5dp);
+                    viewVip.setBackgroundResource(R.drawable.shape_basic1dp_5dp);
+                    textSingle.setTextColor(ContextCompat.getColor(CheLiangXQActivity.this, R.color.light_black));
+                    textVip.setTextColor(ContextCompat.getColor(CheLiangXQActivity.this, R.color.textGold));
+                } else {
+                    viewSingle.setBackgroundResource(R.drawable.shape_basic1dp_5dp);
+                    viewVip.setBackgroundResource(R.drawable.shape_gray1dp_5dp);
+                    textSingle.setTextColor(ContextCompat.getColor(CheLiangXQActivity.this, R.color.textGold));
+                    textVip.setTextColor(ContextCompat.getColor(CheLiangXQActivity.this, R.color.light_black));
+                }
             }
 
             @Override
@@ -201,6 +281,19 @@ public class CheLiangXQActivity extends ZjbBaseActivity implements SwipeRefreshL
                 break;
             default:
                 break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (payVideoDialog != null) {
+            if (payVideoDialog.isShowing()) {
+                payVideoDialog.dismiss();
+            } else {
+                finish();
+            }
+        } else {
+            finish();
         }
     }
 }
