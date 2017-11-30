4d6a6c6a57e706ac8ea6e0213ac343cc4d8dd93e
ZhangJieBo <530092772@qq.com>
2017-11-30 18:01:16 +0800

    2017年11月30日18:00:57

--- seller/src/main/java/hudongchuangxiang/com/seller/fragment/WoDeFragment.java
+++ seller/src/main/java/hudongchuangxiang/com/seller/fragment/WoDeFragment.java
@@ -8,12 +8,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import hudongchuangxiang.com.seller.R;
+import hudongchuangxiang.com.seller.base.ZjbBaseFragment;
+import huisedebi.zjb.mylibrary.customview.HeadZoomScrollView;
+import huisedebi.zjb.mylibrary.util.DpUtils;
+import huisedebi.zjb.mylibrary.util.ScreenUtils;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class WoDeFragment extends Fragment {
+public class WoDeFragment extends ZjbBaseFragment {
 
+
+    private View mInflate;
+    private HeadZoomScrollView scrollView;
+    private View zoomView;
+    private View viewBar;
 
     public WoDeFragment() {
         // Required empty public constructor
@@ -24,7 +33,48 @@ public class WoDeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wo_de, container, false);
+        if (mInflate == null) {
+            mInflate = inflater.inflate(R.layout.fragment_wo_de, container, false);
+            init();
+        }
+        //缓存的rootView需要判断是否已经被加过parent， 如果有parent需要从parent删除，要不然会发生这个rootview已经有parent的错误。
+        ViewGroup parent = (ViewGroup) mInflate.getParent();
+        if (parent != null) {
+            parent.removeView(mInflate);
+        }
+        return mInflate;
     }
 
+    @Override
+    protected void initIntent() {
+
+    }
+
+    @Override
+    protected void initSP() {
+
+    }
+
+    @Override
+    protected void findID() {
+        scrollView = mInflate.findViewById(R.id.scrollView);
+        viewBar = mInflate.findViewById(R.id.viewBar);
+    }
+
+    @Override
+    protected void initViews() {
+        ViewGroup.LayoutParams layoutParams = viewBar.getLayoutParams();
+        layoutParams.height = ScreenUtils.getStatusBarHeight(getActivity()) + (int) DpUtils.convertDpToPixel(45f, getActivity());
+        viewBar.setLayoutParams(layoutParams);
+    }
+
+    @Override
+    protected void setListeners() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
 }
