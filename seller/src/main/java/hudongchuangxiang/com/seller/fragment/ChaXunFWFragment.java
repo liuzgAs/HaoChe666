fc38b06df602fcc405e8ca47204f04078ecac67a
ZhangJieBo <530092772@qq.com>
2017-12-01 15:53:21 +0800

    服务查询

--- seller/src/main/java/hudongchuangxiang/com/seller/fragment/ChaXunFWFragment.java
+++ seller/src/main/java/hudongchuangxiang/com/seller/fragment/ChaXunFWFragment.java
@@ -6,9 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import hudongchuangxiang.com.seller.R;
 import hudongchuangxiang.com.seller.base.ZjbBaseFragment;
+import huisedebi.zjb.mylibrary.util.ScreenUtils;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +19,7 @@ public class ChaXunFWFragment extends ZjbBaseFragment {
 
 
     private View mInflate;
+    private View mRelaTitleStatue;
 
     public ChaXunFWFragment() {
         // Required empty public constructor
@@ -51,12 +54,16 @@ public class ChaXunFWFragment extends ZjbBaseFragment {
 
     @Override
     protected void findID() {
-
+        mRelaTitleStatue = mInflate.findViewById(R.id.relaTitleStatue);
+        ((TextView)mInflate.findViewById(R.id.textViewTitle)).setText("服务查询");
     }
 
     @Override
     protected void initViews() {
-
+        ViewGroup.LayoutParams layoutParams = mRelaTitleStatue.getLayoutParams();
+        layoutParams.height = (int) (getResources().getDimension(R.dimen.titleHeight) + ScreenUtils.getStatusBarHeight(getActivity()));
+        mRelaTitleStatue.setLayoutParams(layoutParams);
+        mRelaTitleStatue.setPadding(0, ScreenUtils.getStatusBarHeight(getActivity()), 0, 0);
     }
 
     @Override
