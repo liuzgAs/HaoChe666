c46715153dba26a9dba4f489c2dc5322ab70dae0
ZhangJieBo <530092772@qq.com>
2017-12-26 09:51:24 +0800

    充值

--- buyer/src/main/java/com/haoche666/buyer/avtivity/PayChongZhiActivity.java
+++ buyer/src/main/java/com/haoche666/buyer/avtivity/PayChongZhiActivity.java
@@ -1,6 +1,9 @@
 package com.haoche666.buyer.avtivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -15,7 +18,12 @@ import com.haoche666.buyer.model.AliPayBean;
 import com.haoche666.buyer.model.CorderRecharge;
 import com.haoche666.buyer.model.OkObject;
 import com.haoche666.buyer.model.PayAlipay;
+import com.haoche666.buyer.model.PayWxpay;
 import com.haoche666.buyer.util.ApiClient;
+import com.tencent.mm.opensdk.constants.Build;
+import com.tencent.mm.opensdk.modelpay.PayReq;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,6 +38,28 @@ public class PayChongZhiActivity extends ZjbBaseActivity implements View.OnClick
     private View[] paySelectView = new View[2];
     private View[] payView = new View[2];
     private int payMode;
+    final IWXAPI api = WXAPIFactory.createWXAPI(this, null);
+    private BroadcastReceiver recevier = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            switch (action) {
+                case Constant.BroadcastCode.PAY_RECEIVER:
+                    cancelLoadingDialog();
+                    int error = intent.getIntExtra("error", -1);
+                    if (error == 0) {
+                        paySuccess();
+                    } else if (error == -1) {
+                        MyDialog.showTipDialog(PayChongZhiActivity.this, "支付失败");
+                    } else if (error == -2) {
+                        MyDialog.showTipDialog(PayChongZhiActivity.this, "支付失败");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -138,6 +168,8 @@ public class PayChongZhiActivity extends ZjbBaseActivity implements View.OnClick
                             case 0:
                                 zhiFuBao(corderRecharge.getOrder_no());
                                 break;
+                            case 1:
+                                weiXinZF(corderRecharge.getOrder_no());
                             default:
                                 break;
                         }
@@ -157,6 +189,84 @@ public class PayChongZhiActivity extends ZjbBaseActivity implements View.OnClick
                 Toast.makeText(PayChongZhiActivity.this, "请求失败", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    /**
+     * des： 网络请求参数
+     * author： ZhangJieBo
+     * date： 2017/8/28 0028 上午 9:55
+     */
+    private OkObject getWXOkObject(String order_no) {
+        String url = Constant.HOST + Constant.Url.PAY_WXPAY;
+        HashMap<String, String> params = new HashMap<>();
+        if (isLogin) {
+            params.put("uid", userInfo.getUid());
+            params.put("tokenTime", tokenTime);
+        }
+        params.put("order_no", order_no);
+        return new OkObject(params, url);
+    }
+
+    /**
+     * des： 微信支付
+     * author： ZhangJieBo
+     * date： 2017/12/25/025 17:25
+     */
+    private void weiXinZF(String order_no) {
+        showLoadingDialog();
+        ApiClient.post(PayChongZhiActivity.this, getWXOkObject(order_no), new ApiClient.CallBack() {
+            @Override
+            public void onSuccess(String s) {
+                cancelLoadingDialog();
+                LogUtil.LogShitou("ChaWeiBaoActivity--微信支付", s + "");
+                try {
+                    PayWxpay payWxpay = GsonUtils.parseJSON(s, PayWxpay.class);
+                    if (payWxpay.getStatus() == 1) {
+                        wechatPay(payWxpay);
+                    } else if (payWxpay.getStatus() == 3) {
+                        MyDialog.showReLoginDialog(PayChongZhiActivity.this);
+                    } else {
+                        Toast.makeText(PayChongZhiActivity.this, payWxpay.getInfo(), Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(PayChongZhiActivity.this, "数据出错", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onError() {
+                cancelLoadingDialog();
+                Toast.makeText(PayChongZhiActivity.this, "请求失败", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     * 微信支付
+     */
+    private void wechatPay(PayWxpay payWxpay) {
+        if (!checkIsSupportedWeachatPay()) {
+            Toast.makeText(PayChongZhiActivity.this, "您暂未安装微信或您的微信版本暂不支持支付功能\n请下载安装最新版本的微信", Toast.LENGTH_SHORT).show();
+        } else {
+            api.registerApp(payWxpay.getAppid());
+            PayReq mPayReq = new PayReq();
+            mPayReq.appId = payWxpay.getAppid();
+            mPayReq.partnerId = payWxpay.getPartnerid();
+            mPayReq.prepayId = payWxpay.getPrepayid();
+            mPayReq.packageValue = payWxpay.getPackageX();
+            mPayReq.nonceStr = payWxpay.getNonceStr();
+            mPayReq.timeStamp = payWxpay.getTimeStamp() + "";
+            mPayReq.sign = payWxpay.getSign().toUpperCase();
+            api.sendReq(mPayReq);
+        }
+    }
+
+    /**
+     * 检查微信版本是否支付支付或是否安装可支付的微信版本
+     */
+    private boolean checkIsSupportedWeachatPay() {
+        boolean isPaySupported = api.getWXAppSupportAPI() >= Build.PAY_SUPPORTED_SDK_INT;
+        return isPaySupported;
     }
 
 
@@ -266,4 +376,17 @@ public class PayChongZhiActivity extends ZjbBaseActivity implements View.OnClick
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Constant.BroadcastCode.PAY_RECEIVER);
+        registerReceiver(recevier, filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(recevier);
+    }
 }
