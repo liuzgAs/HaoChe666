d18a840270526b04ee8137e92bbd0438f5f7432c
ZhangJieBo <530092772@qq.com>
2017-12-28 15:10:32 +0800

    关注车辆

--- buyer/src/main/java/com/haoche666/buyer/viewholder/GuanZuCLViewHolder.java
+++ buyer/src/main/java/com/haoche666/buyer/viewholder/GuanZuCLViewHolder.java
@@ -2,7 +2,11 @@ package com.haoche666.buyer.viewholder;
 
 import android.support.annotation.LayoutRes;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.haoche666.buyer.R;
 import com.haoche666.buyer.model.AttentionGetattention;
 import com.jude.easyrecyclerview.adapter.BaseViewHolder;
 
@@ -11,13 +15,34 @@ import com.jude.easyrecyclerview.adapter.BaseViewHolder;
  */
 public class GuanZuCLViewHolder extends BaseViewHolder<AttentionGetattention.DataBean> {
 
+    private final ImageView imageImg;
+    private final ImageView imageIs_sale;
+    private final TextView textTitle;
+    private final TextView textDes;
+
     public GuanZuCLViewHolder(ViewGroup parent, @LayoutRes int res) {
         super(parent, res);
+        imageImg = $(R.id.imageImg);
+        imageIs_sale = $(R.id.imageIs_sale);
+        textTitle = $(R.id.textTitle);
+        textDes = $(R.id.textDes);
     }
 
     @Override
     public void setData(AttentionGetattention.DataBean data) {
         super.setData(data);
+        Glide.with(getContext())
+                .load(data.getImg())
+                .asBitmap()
+                .placeholder(R.mipmap.ic_empty)
+                .into(imageImg);
+        if (data.getIs_sale()==1){
+            imageIs_sale.setImageResource(R.mipmap.yishou);
+        }else {
+            imageIs_sale.setImageResource(R.mipmap.daishou);
+        }
+        textTitle.setText(data.getTitle());
+        textDes.setText(data.getDes());
     }
     
 }
